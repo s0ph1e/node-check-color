d5e412f04f0f3158b2d3076a55fa8f23eb502df2
s0ph1e <sonya1912@gmail.com>
2015-08-19 00:02:18 +0300

    Init

---
+++ shades.js
@@ -0,0 +1,24 @@
+// http://www.workwithcolor.com/color-names-01.htm
+var shades = {
+	'red': [
+		{ h: [355, 360] },
+		{ h: [0, 10] }
+	],
+	'red-orange': [{ h: [11, 20] }],
+	'orange-brown': [{ h: [21, 40] }],
+	'orange-yellow': [{ h: [41, 50] }],
+	'yellow': [{ h: [51, 60] }],
+	'yellow-green': [{ h: [61, 80] }],
+	'green': [{ h: [81, 140] }],
+	'green-cyan': [{ h: [141, 169] }],
+	'cyan': [{ h: [170, 200] }],
+	'cyan-blue': [{ h: [201, 220]}],
+	'blue': [{ h: [221, 240]}],
+	'blue-magenta': [{ h: [241, 280]}],
+	'magenta': [{ h: [281, 320]}],
+	'magenta-pink': [{ h: [321, 330]}],
+	'pink': [{ h: [331, 345]}],
+	'pin-red': [{ h: [346, 354]}]
+};
+
+module.exports = shades;
